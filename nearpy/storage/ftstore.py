c217c38d00cc280966d8e50f6cf659f035ac4014
runrunliuliu <liuliu.tju@gmail.com>
2017-06-09 11:58:53 +0800

    add rocks

---
+++ nearpy/storage/ftstore.py
@@ -0,0 +1,58 @@
+import logging
+import rocksdb
+import cPickle as pickle
+
+
+logger  = logging.getLogger('FTStore') 
+
+
+class FTStore(object):
+
+    def __init__(self, mode='MEM'):
+        self.store = None
+        self.mode  = mode
+
+        if mode == 'MEM':
+            self.store = dict()
+        if mode == 'ROCKS':
+            self.store = self.initRocks()
+
+    def wrap(self, string):
+        return "b'" + string + "'"
+
+    def add(self, key, val):
+        if self.mode == 'MEM':
+            self.store[key] = val
+        if self.mode == 'ROCKS':
+            self.store.put(self.wrap(key), self.wrap(val))
+
+    def get(self, key):
+        if self.mode == 'MEM':
+            return self.store[key]
+        if self.mode == 'ROCKS':
+            vals = self.store.get(self.wrap(key))
+            return pickle.loads(vals)
+        
+    def contains(self, key):
+        ret = True
+        if self.mode == 'MEM':
+            if key not in self.store:
+                ret = False
+        return ret
+
+    def initRocks(self):
+        opts = rocksdb.Options()
+        opts.create_if_missing = True
+        opts.max_open_files = 300000
+        opts.write_buffer_size = 67108864
+        opts.max_write_buffer_number = 3
+        opts.target_file_size_base = 67108864
+
+        opts.table_factory = rocksdb.BlockBasedTableFactory(
+            filter_policy=rocksdb.BloomFilterPolicy(10),
+            block_cache=rocksdb.LRUCache(2 * (1024 ** 3)),
+            block_cache_compressed=rocksdb.LRUCache(500 * (1024 ** 2)))
+
+        db = rocksdb.DB("test.db", opts)
+        return db
+#
